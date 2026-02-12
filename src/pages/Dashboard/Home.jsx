9380e7f9a086633bfac512ef4fb04489f66dc26e
SamiTahir09 <samitahir858@gmail.com>
2026-02-11 21:23:25 -0800

    nip

--- src/pages/Dashboard/Home.jsx
+++ src/pages/Dashboard/Home.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import DashboardLayout from '../../components/Layout/DashboardLayout'
 import { useUserAuth } from '../../hooks/useUserAuth'
+import { useNavigate } from 'react-router-dom';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPath';
 
 const Home = () => {
     useUserAuth();
+
+    const navigate = useNavigate();
+    const [dashboardData, setDashboardData] = React.useState(null);
+    const [loading, setLoading] = React.useState(false);
+
+
+    const fetchDashboardData = async () => {
+        if (loading) {
+            setLoading(true);
+        }
+
+        try {
+            const response = await axiosInstance.get(`${API_PATHS.DASHBOARD.GET_DATA}`);
+            if (response.data) {
+                setDashboardData(response.data)
+            }
+        } catch (error) {
+
+        }
+    }
     return (
         <DashboardLayout activeMenu="Dashboard">
             <div className='my-5 mx-auto'>
